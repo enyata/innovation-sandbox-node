cc99bd399db457e5c54854668d51fa99878c64d8
Akinpelumi Akintunde <akintundeakinpelumi@gmail.com>
2020-10-23 20:04:20 +0100

    Update Swiftgpiforcorporates.test.js

--- test/swift/Swiftgpiforcorporates.test.js
+++ test/swift/Swiftgpiforcorporates.test.js
@@ -2,9 +2,9 @@ const chai = require('chai');
 const nock = require('nock');
 const faker = require('faker');
 
-const SwiftGpiforCorporate = require('../../lib/swift/SwiftGpiForCorporates');
+const SwiftGpiforCorporate = require('../../lib/swift/Swiftgpiforcorporates');
 
-const SwiftGpiforCorporateMock = require('../mock/swift/SwiftGpiForCorporates');
+const SwiftGpiforCorporateMock = require('../mock/swift/Swiftgpiforcorporates');
 
 const { data } = SwiftGpiforCorporateMock;
 
